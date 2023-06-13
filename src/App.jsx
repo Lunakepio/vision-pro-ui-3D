239702af9335780fa04b78eb6c275ed04e258ad1
Lunakepio <alex@maxime-benard.Fr>
2023-06-13 18:53:32 +0200

    updated package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useLoader } from "@react-three/fiber";
 import React, { useEffect, useRef, useState, useTransition } from "react";
-import { LUTCubeLoader, LUTEffect } from 'postprocessing'
+import { LUTCubeLoader, LUTEffect } from "postprocessing";
 import { Model } from "./Apple_vision_pro";
 import {
   AccumulativeShadows,
@@ -17,14 +17,27 @@ import {
   Html,
   PerspectiveCamera,
   DeviceOrientationControls,
+  FaceControls,
+  FaceLandmarker,
 } from "@react-three/drei";
 
 import { useControls } from "leva";
 import { ModelTwo } from "./Apple_vision_pro_2023";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { EffectComposer, N8AO, SSR, Bloom, LUT, SMAA } from "@react-three/postprocessing";
+import {
+  EffectComposer,
+  N8AO,
+  SSR,
+  Bloom,
+  LUT,
+  SMAA,
+  Autofocus,
+} from "@react-three/postprocessing";
 import * as THREE from "three";
 import "./globals.scss";
+import { SpatialUI } from "./SpatialUI";
+import { Player } from "./Player";
+import { SideMenu } from "./SideMenu";
 
 function App() {
   function Env() {
@@ -33,7 +46,7 @@ function App() {
     // For instance by showing a message
     const [inTransition, startTransition] = useTransition();
     const { blur } = useControls({
-      blur: { value: 50, min: 0, max: 1 },
+      blur: { value: 0, min: 0, max: 1 },
       preset: {
         value: preset,
         options: [
@@ -59,77 +72,56 @@ function App() {
 
   function Camera() {
     const ref = useRef();
-  
-    useEffect(() => {
-      const handleDeviceOrientation = (e) => {
-        // Normalisez les valeurs d'orientation ici si nécessaire
-        // ref.current.rotation.y = e.gamma / 90; // divisez par 90 pour normaliser à 1
 
-        ref.current.position.y = 2 + (e.beta / 90) * 5 ;
-        ref.current.lookAt(0, 0, 0);
-      };
-  
-      window.addEventListener("deviceorientation", handleDeviceOrientation);
-  
-      // Assurez-vous de supprimer l'écouteur d'événements lorsque le composant est démonté
-      return () => {
-        window.removeEventListener("deviceorientation", handleDeviceOrientation);
-      };
-    }, []);
-  
     // Vous n'avez pas besoin d'appeler updateProjectionMatrix ici à moins que fov ou d'autres propriétés ne changent
-    return <PerspectiveCamera ref={ref} makeDefault near={0.1} far={2000} position={[0, 2,10]} fov={45} />;
+    return (
+      <>
+        <PerspectiveCamera
+          ref={ref}
+          makeDefault
+          near={0.1}
+          far={2000}
+          fov={90}
+        />
+      </>
+    );
   }
-  
 
   let yearStart = 2011;
-
+  const face = useRef();
+  useEffect(() => {
+    if(face.current){
+      console.log(face.current.faceMeshApiRef)
+    }
+  }, [face])
   const texture = useLoader(LUTCubeLoader, "./Titanium_Dream_03.cube");
   return (
     <div className="App" style={{ width: "100%", height: "100vh" }}>
       <div className="" style={{ width: "100vw", height: "100vh" }}>
-        <Canvas
-        >
-          
-            <ScrollControls pages={3}>
+        <Canvas>
+          {/* <ScrollControls pages={3}>
 
               <ModelTwo position={[0, 0.25, 0]} scale={10}/>
 
-            </ScrollControls>
-            <Camera />
-          {/* <Html transform>
-            <div className="photos">
-              {[...Array(12)].map((e, i) => {
-                yearStart++;
-                let id = Math.floor(Math.random() * 1000);
-                return (
-                  <div className="photo" key={i}>
-                    <p>{yearStart}</p>
-                    <img
-                      src={`https://picsum.photos/seed/${id}/200/113`}
-                      alt=""
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </Html>
-          <Html transform position={[0, -11.8,4]}>
-            <div className="container">
-              <button className="selected">Years</button>
-              <button>Months</button>
-              <button>Days</button>
-              <button>All Photos</button>
-            </div>
-          </Html> */}
+            </ScrollControls> */}
+
           {/* <OrbitControls enableZoom={false} /> */}
-          <EffectComposer>
-            
-            <SMAA />
-            <LUT lut={texture} />
-          </EffectComposer>
-          <Env />
-          <DeviceOrientationControls />
+          <FaceLandmarker>
+            <FaceControls eyes ref={face} offsetScalar={15} />
+            <Env />
+            <group position={[0,0, -20]}>
+            <Html transform>
+            <SpatialUI />
+            </Html>
+            <Html transform position={[0, -10.5, 5]}>
+            <Player />
+            </Html>
+            <Html transform position={[-17,0,10]}>
+            <SideMenu />
+            </Html>
+            </group>
+            <DeviceOrientationControls />
+          </FaceLandmarker>
         </Canvas>
       </div>
     </div>
